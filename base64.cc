0d2ffb3625dd291ab401f723a6a3f26a9d1e26fb
paulrevere4 <revereiv@gmail.com>
2015-09-28 18:07:45 -0400

    Added base64.cc with function stubs

---
+++ base64.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+
+/*
+Function base64Encode:
+	Encodes a series of bytes specified by the arguments into a string of Base64
+	chars that will represent them
+
+Arguments:
+	char* input - pointer to start of byte string to be encoded
+	long len - number of bytes to be encoded
+
+Returns:
+	std::string - Base64 encoded string of the input bytes
+
+*/
+std::string base64Encode(	const unsigned char* input,
+							const unsigned long len){
+	std::string encoded;
+
+	return encoded;
+}
+
+/*
+Function base64Decode:
+	Decodes a string of Base64 chars to the bytes they represent
+
+Arguments:
+	std::string encoded - Base64 encoded string
+
+Returns:
+	std::string - decoded string of bytes from the encoded data
+*/
+std::string base64Decode(std::string encoded){
+	std::string decoded;
+	
+	return decoded;
+}
+
+
+int main(int argc, char** argv){
+
+	return 0;
+}
+
+
+
+
+
